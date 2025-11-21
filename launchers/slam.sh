444c5e9a7443894bc6e5fb39a053a9f04a182e82
Jaydan Aladro <jaydaninparis@gmail.com>
2025-11-20 21:39:45 -0500

    merging a recipe in it

--- launchers/slam.sh
+++ launchers/slam.sh
@@ -3,14 +3,13 @@ set -e
 
 source /environment.sh
 
-# Initialize launchfile (Duckietown helper)
-dt-launchfile-init
+source /opt/ros/noetic/setup.bash
+source /code/devel/setup.bash --extend
 
-rosrun slam apriltag_ekf_slam_node.py \
-    _image_topic:=camera_node/image/compressed \
-    _odom_topic:=velocity_to_pose_node/pose \
-    _camera_info_topic:=camera_node/camera_info \
-    _tag_size:=0.065
+# rosrun slam apriltag_ekf_slam_node.py \
+#     _image_topic:=camera_node/image/compressed \
+#     _odom_topic:=velocity_to_pose_node/pose \
+#     _camera_info_topic:=camera_node/camera_info \
+#     _tag_size:=0.065
 
-# Wait for node to exit
-dt-launchfile-join
+exec roslaunch slam apriltag_ekf_slam_node.launch veh:=$VEHICLE_NAME
